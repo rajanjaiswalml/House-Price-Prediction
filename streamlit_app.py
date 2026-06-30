bc51b0a4d46eb968a560189fa3c062ccfe4175b2
Rajan Jaiswal <rajanjai2002@gmail.com>
2026-06-30 23:33:38 +0530

    Update API URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 
 # ----------------- CONFIG -----------------
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://house-price-api-ekwk.onrender.com"
 
 st.set_page_config(
     page_title="House Price Prediction",
